11739d8b2a2dd6092856369c83c9c95a891d30b0
Martin Jesper Low Madsen <martin@martinjlowm.dk>
2018-05-06 22:43:29 +0200

    Initial commit

---
+++ media.lua
@@ -0,0 +1,10 @@
+gxMedia = {
+    auraFont = 'Interface\\AddOns\\gxMedia\\media\\squares.ttf',
+    bgFile = 'Interface\\ChatFrame\\ChatFrameBackground',
+    buttonOverlay = 'Interface\\AddOns\\gxMedia\\media\\buttonoverlay',
+    edgeFile = 'Interface\\AddOns\\gxMedia\\media\\backdropedge',
+    font = 'Interface\\AddOns\\gxMedia\\media\\russel_square_lt.ttf',
+    glow = 'Interface\\AddOns\\gxMedia\\media\\glow.tga',
+    statusBar = 'Interface\\AddOns\\gxMedia\\media\\statusbar.tga',
+    symbolFont = 'Interface\\AddOns\\gxMedia\\media\\PIZZADUDEBULLETS.ttf'
+}
